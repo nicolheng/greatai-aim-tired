619d3ac19f7a912d231625d9a12d4ebd319075bd
nicolheng <nicolhengsiyi@gmail.com>
2025-09-20 00:01:06 +0800

    remove properties route cuz not needed anymore

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
-import propertiesRoutes from "./routes/properties.js";
 
 dotenv.config();
 
@@ -11,7 +10,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
-app.use("/api/properties", propertiesRoutes);
 
 //testing purpose :D
 app.get("/", (req, res) => {
