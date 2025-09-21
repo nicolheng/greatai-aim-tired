1f489fc0b8111a715bc9b10a4b0dc1fdc352e658
INJY <178879836+IvenNJY@users.noreply.github.com>
2025-09-22 01:20:05 +0800

    Delete routes directory

--- routes/questionaire.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submitQuestionaire } from "../controllers/questionaireControllers.js"
-
-const router = express.Router();
-
-router.post("/", submitQuestionaire);
-
-export default router;
